ec1db864d1e214c2ec958890eaffc9b1e012c15c
GRE1DY <gavin.hensley.1@gmail.com>
2025-05-02 23:35:37 -0400

    Initial commit

---
+++ src/main/java/net/gavin/zoocraftmod/ZoocraftModClient.java
@@ -0,0 +1,11 @@
+package net.gavin.zoocraftmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ZoocraftModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/gavin/zoocraftmod/ZoocraftModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.gavin.zoocraftmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ZoocraftModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
